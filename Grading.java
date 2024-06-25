23c5d3af7d601c99c918048a7d72c8a978430846
Alice <alice@example.com>
2024-06-25 23:17:44 +0300

    Add Final Code by Ahmad

--- Grading.java
+++ Grading.java
@@ -5,8 +5,12 @@ public class Grading{
     Scanner scanner = new Scanner(System.in);
     double grade = scanner.nextDouble();
     System.out.println(FinalGrade(grade));
+    System.out.println(FinalCode(grade));
   }
   public static float FinalGrade(double grade){
     return (float) ((grade * 4.2) / 100); 
+  }
+  public static String FinalCode(double grade){
+    return grade < 50 ? 'F' : grade < 60 ? (grade < 54 ? "D-" : grade < 57 ? "D" : "D+") : grade < 70 ? (grade < 64 ? "C-" : grade < 67 ? "C" : "C+") : grade < 80 ? (grade < 74 ? "B-" : grade < 77 ? "B" : "B+") : (grade < 64 ? "A-" : grade < 95 ? "A" : "A+");
   }
 }
