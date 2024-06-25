611f22b4097199916fb0b0b9c74f9e9c5e51a2e5
Alice <alice@example.com>
2024-06-25 23:13:33 +0300

    Add Grade by abd

--- Grading.java
+++ Grading.java
@@ -1,1 +1,12 @@
-"public class Grading { public static void main(String[] args) { System.out.println('Grading System'); } }" 
+import java.util.Scanner;
+public class Grading{
+  public static void main(String[] args) {
+    System.out.println("Grading System Initialized");
+    Scanner scanner = new Scanner(System.in);
+    double grade = scanner.nextDouble();
+    System.out.println(FinalGrade(grade));
+  }
+  public static float FinalGrade(double grade){
+    return (float) ((grade * 4.2) / 100); 
+  }
+}
